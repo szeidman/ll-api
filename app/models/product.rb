3f8fcefc8b0275665af8607988acd5b4e942734a
szeidman <sam.zeidman@gmail.com>
2018-02-21 17:29:12 -0500

    Add presence and numericality validations to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
    has_attached_file :image
    do_not_validate_attachment_file_type :image
+
+   validates_presence_of :name, :description
+   validates_numericality_of :price
+   
 end
