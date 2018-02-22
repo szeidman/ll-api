3896c91ce9dccbae2a3dd8024b02bc1ba478affa
szeidman <sam.zeidman@gmail.com>
2018-02-22 11:28:51 -0500

    Begin debugging Paperclip uploads, update README

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -81,6 +81,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :price, :image_base)
+      params.require(:product).permit(:name, :description, :price, :image)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-   before_validation :parse_picture
-   attr_accessor :image_base
+   # before_validation :parse_picture
+   # attr_accessor :image_base
 
    has_attached_file :image
    do_not_validate_attachment_file_type :image
@@ -11,12 +11,12 @@ class Product < ApplicationRecord
    validates_presence_of :name, :description
    validates_numericality_of :price
 
-   private
-    def parse_picture
-      picture = Paperclip.io_adapters.for(image_base)
-      picture.original_filename = "file.jpg"
-      self.image = picture
-    end
+  #  private
+  #   def parse_picture
+  #     picture = Paperclip.io_adapters.for(image_base)
+  #     picture.original_filename = "file.jpg"
+  #     self.image = picture
+  #   end
     ## Adapted from https://www.pluralsight.com/guides/ruby-ruby-on-rails/handling-file-upload-using-ruby-on-rails-5-api
 
 end
